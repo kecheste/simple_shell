8891c779e408da892f83c3e97e79bc0cbab065aa
kecheste <aben.ezer@gmail.com>
2022-05-19 03:47:10 +0300

    g

--- helper.c
+++ helper.c
@@ -1,161 +1,103 @@
-/*
- * File: helper.c
- * Auth: Abtsega Tesfaye
- * 	Dimru Dimru
- */
-
 #include "shell.h"
 
-void free_args(char **args, char **front);
-char *get_pid(void);
-char *get_env_value(char *beginning, int len);
-void variable_replacement(char **args, int *exe_ret);
-
 /**
- * free_args - Frees up memory taken by args.
- * @args: A null-terminated double pointer containing commands/arguments.
- * @front: A double pointer to the beginning of args.
+ * tokenizer - tokenizes input and stores it into an array
+ *@input_string: input to be parsed
+ *@delim: delimiter to be used, needs to be one character string
+ *
+ *Return: array of tokens
  */
-void free_args(char **args, char **front)
+
+char **tokenizer(char *input_string, char *delim)
 {
-	size_t i;
+	int num_delim = 0;
+	char **av = NULL;
+	char *token = NULL;
+	char *save_ptr = NULL;
 
-	for (i = 0; args[i] || args[i + 1]; i++)
-		free(args[i]);
+	token = _strtok_r(input_string, delim, &save_ptr);
 
-	free(front);
+	while (token != NULL)
+	{
+		av = _realloc(av, sizeof(*av) * num_delim, sizeof(*av) * (num_delim + 1));
+		av[num_delim] = token;
+		token = _strtok_r(NULL, delim, &save_ptr);
+		num_delim++;
+	}
+
+	av = _realloc(av, sizeof(*av) * num_delim, sizeof(*av) * (num_delim + 1));
+	av[num_delim] = NULL;
+
+	return (av);
 }
 
 /**
- * get_pid - Gets the current process ID.
- * Description: Opens the stat file, a space-delimited file containing
- *              information about the current process. The PID is the
- *              first word in the file. The function reads the PID into
- *              a buffer and replace the space at the end with a \0 byte.
+ *print - prints a string to stdout
+ *@string: string to be printed
+ *@stream: stream to print out to
  *
- * Return: The current process ID or NULL on failure.
+ *Return: void, return nothing
  */
-char *get_pid(void)
+void print(char *string, int stream)
 {
-	size_t i = 0;
-	char *buffer;
-	ssize_t file;
+	int i = 0;
 
-	file = open("/proc/self/stat", O_RDONLY);
-	if (file == -1)
+	for (; string[i] != '\0'; i++)
+		write(stream, &string[i], 1);
+}
+
+/**
+ *remove_newline - removes new line from a string
+ *@str: string to be used
+ *
+ *
+ *Return: void
+ */
+
+void remove_newline(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		perror("Cant read file");
-		return (NULL);
-	}
-	buffer = malloc(120);
-	if (!buffer)
-	{
-		close(file);
-		return (NULL);
-	}
-	read(file, buffer, 120);
-	while (buffer[i] != ' ')
+		if (str[i] == '\n')
+			break;
 		i++;
-	buffer[i] = '\0';
-
-	close(file);
-	return (buffer);
+	}
+	str[i] = '\0';
 }
 
 /**
- * get_env_value - Gets the value corresponding to an environmental variable.
- * @beginning: The environmental variable to search for.
- * @len: The length of the environmental variable to search for.
+ *_strcpy - copies a string to another buffer
+ *@source: source to copy from
+ *@dest: destination to copy to
  *
- * Return: If the variable is not found - an empty string.
- *         Otherwise - the value of the environmental variable.
- *
- * Description: Variables are stored in the format VARIABLE=VALUE.
+ * Return: void
  */
-char *get_env_value(char *beginning, int len)
+
+void _strcpy(char *source, char *dest)
 {
-	char **var_addr;
-	char *replacement = NULL, *temp, *var;
+	int i = 0;
 
-	var = malloc(len + 1);
-	if (!var)
-		return (NULL);
-	var[0] = '\0';
-	_strncat(var, beginning, len);
-
-	var_addr = _getenv(var);
-	free(var);
-	if (var_addr)
-	{
-		temp = *var_addr;
-		while (*temp != '=')
-			temp++;
-		temp++;
-		replacement = malloc(_strlen(temp) + 1);
-		if (replacement)
-			_strcpy(replacement, temp);
-	}
-
-	return (replacement);
+	for (; source[i] != '\0'; i++)
+		dest[i] = source[i];
+	dest[i] = '\0';
 }
 
 /**
- * variable_replacement - Handles variable replacement.
- * @line: A double pointer containing the command and arguments.
- * @exe_ret: A pointer to the return value of the last executed command.
+ *_strlen - counts string length
+ *@string: string to be counted
  *
- * Description: Replaces $$ with the current PID, $? with the return value
- *              of the last executed program, and envrionmental variables
- *              preceded by $ with their corresponding value.
+ * Return: length of the string
  */
-void variable_replacement(char **line, int *exe_ret)
-{
-	int j, k = 0, len;
-	char *replacement = NULL, *old_line = NULL, *new_line;
 
-	old_line = *line;
-	for (j = 0; old_line[j]; j++)
-	{
-		if (old_line[j] == '$' && old_line[j + 1] &&
-				old_line[j + 1] != ' ')
-		{
-			if (old_line[j + 1] == '$')
-			{
-				replacement = get_pid();
-				k = j + 2;
-			}
-			else if (old_line[j + 1] == '?')
-			{
-				replacement = _itoa(*exe_ret);
-				k = j + 2;
-			}
-			else if (old_line[j + 1])
-			{
-				/* extract the variable name to search for */
-				for (k = j + 1; old_line[k] &&
-						old_line[k] != '$' &&
-						old_line[k] != ' '; k++)
-					;
-				len = k - (j + 1);
-				replacement = get_env_value(&old_line[j + 1], len);
-			}
-			new_line = malloc(j + _strlen(replacement)
-					  + _strlen(&old_line[k]) + 1);
-			if (!line)
-				return;
-			new_line[0] = '\0';
-			_strncat(new_line, old_line, j);
-			if (replacement)
-			{
-				_strcat(new_line, replacement);
-				free(replacement);
-				replacement = NULL;
-			}
-			_strcat(new_line, &old_line[k]);
-			free(old_line);
-			*line = new_line;
-			old_line = new_line;
-			j = -1;
-		}
-	}
+int _strlen(char *string)
+{
+	int len = 0;
+
+	if (string == NULL)
+		return (len);
+	for (; string[len] != '\0'; len++)
+		;
+	return (len);
 }
